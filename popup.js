a8a05905a2334d57c50777784eecb0e413d1a589
Varun2948 <deepak.pradhan048@gmail.com>
2019-11-29 12:40:17 +0545

    Adds website title

--- popup.js
+++ popup.js
@@ -1,14 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-  //var checkPageButton = document.getElementById('checkPage');
-  //checkPageButton.addEventListener('click', function() {
-	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-	    var url = tabs[0].url;
+document.addEventListener('DOMContentLoaded', function () {
+	//var checkPageButton = document.getElementById('checkPage');
+	//checkPageButton.addEventListener('click', function() {
+	chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
+		var url = tabs[0].url;
+
+		document.getElementById('url').innerHTML = url
 		document.getElementById("qrcode").innerHTML = "";
-	    	var qrcode = new QRCode("qrcode");
+
+		var qrcode = new QRCode("qrcode");
 		qrcode.makeCode(url);
-		
+
 	});
 
-    
- // }, false);
+
+	// }, false);
 }, false);
